39d677ddddf3532297658eb4350024002b27c6f5
krako <40539800+krakozaure@users.noreply.github.com>
2018-07-24 21:16:42 +0200

    Update to 0.0.4
    Fix typos
    Fix bug with filesystem encoding on print

--- fsscan/cli.py
+++ fsscan/cli.py
@@ -1,13 +1,23 @@
 import argparse
+import sys
 
 from .core import run as run_scan
+from .__version__ import __version__
 
 
 def run():
 
     parser = argparse.ArgumentParser(
-        description="Quickly scan a directory and yield matching entries.",
+        description="Quickly scan a directory and yield matching entries,"
+                    " based on patterns and/or entry type.",
         prog="fsscan",
+    )
+
+    pyversion = ".".join(str(v) for v in sys.version_info[:3])
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s {} (python {})".format(__version__, pyversion),
     )
 
     parser.add_argument(
@@ -51,9 +61,20 @@ def run():
     args = parser.parse_args()
 
     entries = run_scan(
-       directory=args.d,
-       patterns=args.p, ignore_case=args.i, wanted_type=args.t,
-       recursive=args.r, on_error=None, follow_links=args.f, callback=None
+        directory=args.d,
+        patterns=args.p,
+        ignore_case=args.i,
+        wanted_type=args.t,
+        recursive=args.r,
+        on_error=None,
+        follow_links=args.f,
+        callback=None,
     )
+
+    fse = sys.getfilesystemencoding()
     for entry in entries:
-        print(entry)
+        try:
+            print(entry)
+        except UnicodeEncodeError:
+            entry = entry.encode(fse, errors="replace").decode(fse)
+            print(entry)
