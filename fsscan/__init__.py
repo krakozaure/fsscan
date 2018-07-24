508ef1979707086a9e11683812a4ee5ef8e105f1
krako <40539800+krakozaure@users.noreply.github.com>
2018-07-24 21:19:03 +0200

    Update to 0.0.4
    Add __all__ variable

--- fsscan/__init__.py
+++ fsscan/__init__.py
@@ -1,14 +1,25 @@
 
-from .core import (  # noqa
+from .core import (
     BOTH_TYPES,
     DIR_TYPE,
     FILE_TYPE,
     run,
 )
 
-from .callbacks import (  # noqa
+from .callbacks import (
     cast_to_Path,
     cast_to_Path_str,
     cast_to_str,
     convert_entries,
 )
+
+__all__ = [
+    "BOTH_TYPES",
+    "DIR_TYPE",
+    "FILE_TYPE",
+    "run",
+    "cast_to_Path",
+    "cast_to_Path_str",
+    "cast_to_str",
+    "convert_entries",
+]
