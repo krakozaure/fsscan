cd56edd7f550778c8bb6f1cb822ecd99978dec27
Krakozaure <krakozaure@protonmail.com>
2022-01-08 15:07:00 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fsscan",
-    version="0.0.4",
+    version="0.0.5",
     author="krakozaure",
     license="MIT",
     author_email="",
